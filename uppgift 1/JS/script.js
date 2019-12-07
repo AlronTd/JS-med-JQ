7108841b23358caa65886de817e16b064670bece
Albin Jynnesjö <albin@jynnesjo.se>
2019-12-07 14:43:23 +0100

    2019-12-07 14:43

--- uppgift 1/JS/script.js
+++ uppgift 1/JS/script.js
@@ -5,6 +5,119 @@ $(function () {
         const matchSuffix = '2'
         
         
+        function setInvalid(elem) {
+            elem.classList.remove('is-valid')
+            elem.classList.add('is-invalid')
+        }
+        
+        function setValid(elem, willVisualise) {
+            elem.classList.remove('is-invalid')
+            if(willVisualise){
+                elem.classList.add('is-valid')
+            }
+        }
+        
+        function checkMatch(e, elem, suffix){
+            
+        }
+        
+        
+        // represents a group of regular expressions
+        class regExpCollection{
+            constructor(regexArray) {
+                this.collection = regexArray
+                
+                // returns true if any of the expressions are matched in the input string,
+                // else returns false.
+                this.testAny = (string) => {
+                    for(let exp in this.collection){
+                        if(exp.test(string)){
+                            return true
+                        }
+                    }
+                    return false
+                }
+                
+                // returns true if all of the expressions are matched in the input string,
+                // else returns false.
+                this.testAll = (string) => {
+                    for(let exp in this.collection){
+                        if(!(exp.test(string))){
+                            return false
+                        }
+                    }
+                    return true
+                }
+                
+                
+                // returns an array of all substrings that any of the expressions match.
+                this.exec = (string) => {
+                    let result = []
+                    for(let exp in this.collection){
+                        result = result.concat(exp.exec(string))
+                    }
+                    return result
+                }
+                
+                // adds a regular expression to the collection.
+                this.add = (regex) => this.collection.push(regex)
+            }
+        }
+        
+        class validator {
+            constructor(elem) {
+                
+                this.element = elem
+                this.toValidate = []    // array of functions
+                
+                this.badChars = new regExpCollection([/[<>/]/])
+                
+                for(let HTMLClass of elem.classList){
+                    switch (HTMLClass) {
+                        case 'no-validate':
+                            this.validate = (e) => {}
+                            this.limitCharInput = (e) => {}
+                            return;
+                        case 'word': 
+                            this.badChars.add(/^(?!.*([0-9]|[\u0041-\u005A]|[\u0061-\u007A]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u00FF]|\s))/)
+                            this.toValidate.push((e) => validateWord(e, this.element, this.badChars))
+                            break;
+                        case 'passwd':
+                            this.badChars.add(/^(?!.*([0-9]|[\u003F-\u005A]|[\u0061-\u007A]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u00FF]|\u0021|[\u0023-\u0026]|\u005F|\s))/)
+                            this.toValidate.push((e) => validatePasswd(e, this.element, this.badChars))
+                            break;
+                        case 'email':
+                            this.toValidate.push((e) => validateEmail(e, this.element))
+                            break;
+                        case 'match':
+                            this.toValidate.push((e) => checkMatch(e, this.element, matchSuffix))
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+                this.limitCharInput = (e) => preventBadInput(e, this.badChars)
+                
+                this.validate = (e) => {
+                    for(let func of this.toValidate){
+                        func(e)
+                    }
+                }
+
+            }
+        }
+
+
+        for (let elem of $('form input, form textarea')) {
+            console.log(elem)
+            console.log(elem.classList)
+        }
+
+    }
+    main();
+});
+
         // function isTextField(elem) {
 
         //     const nonTextTypes = ['button', 'radio', 'checkbox', 'color', 'hidden', 'image', 'reset', 'submit']
@@ -21,52 +134,3 @@ $(function () {
         //         default: return false;
         //     }
         // }
-
-
-
-        class validator {
-            constructor(elem) {
-                const validationClasses = [  , ]
-                
-                this.element = elem
-                for(let HTMLClass of elem.classList){
-                    switch (HTMLClass) {
-                        case 'no-validate':
-                            this.validate = () => {}
-                            this.limitCharInput = () => {}
-                            return;
-                        case 'word': 
-                            this.upperCharLimit = 40
-                            this.badChars = /([^0-9\u0041-\u005A\u0061-\u007A\u00C0-00D6\u00D8-00F6\u00F8-00FF]|\s)+/
-                            this.validate = (e) => validateWord(e, this.element, this.upperCharLimit,  this.badChars)
-                            break;
-                        case 'passwd':
-                            this.upperCharLimit = 40
-                            this.badChars = /([^0-9\u003F-\u005A\u0061-\u007A\u00C0-00D6\u00D8-00F6\u00F8-00FF\u0021\u0023-\u0026\u005F]|\s)+/
-                            this.validate = (e) => validatePasswd(e, this.element, this.upperCharLimit,  this.badChars)
-                            break;
-                        case 'match':
-                            
-                            break;
-                        case 'email':
-                            
-                            break;
-                        default:
-                            throw new Error('Feature not implemented!');
-                    }
-                }
-                
-                this.limitCharInput = (e) => preventBadInput(e, this.upperCharLimit, this.badChars)
-
-            }
-        }
-
-
-        for (let elem of $('form input, form textarea')) {
-            console.log(elem)
-            console.log(elem.classList)
-        }
-
-    }
-    main();
-});
